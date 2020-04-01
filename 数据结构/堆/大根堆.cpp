7326ca3e3bf7bfdcfb4fac7467c180b1f69d9650
bjq2007 <59324916+bjq2007@users.noreply.github.com>
2020-04-01 20:08:29 +0800

    Create 大根堆.cpp

---
+++ 数据结构/堆/大根堆.cpp
@@ -0,0 +1,45 @@
+const int N = 100010;
+
+int heap[N], s = 0;
+
+void down(int u) {
+	while ((u << 1) <= s) {
+		int v = u << 1;
+		if (v < n && heap[v + 1] > heap[v]) v++;
+		if (heap[v] <= heap[u]) break;
+		swap(heap[u], heap[v]);
+		u = v;
+	}
+}
+
+void up(int u) {
+	while (u > 1 && heap[u] > heap[fa(u)]) {
+		swap(heap[u], heap[fa(u)]);
+		u = fa(u);
+	}
+}
+
+void pop() {
+	heap[1] = heap[s--];
+	down(1);
+}
+
+int top() {
+	return heap[1];
+}
+
+void insert(int x) {
+	heap[++s] = x;
+	up(s);
+}
+
+void build() {
+	for (int i = (n >> 1); i; i--) down(i);
+}
+
+insert(x); // 往堆里插入数x 
+top(); // 堆中的最大值
+pop(); // 删除堆中最大值
+heap[k] = heap[s--]; down(k); up(k); // 删除第k位的元素
+heap[k] = x; down(k); up(k); // 修改第k位的元素为x 
+build(); // 建堆 
